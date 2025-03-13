26b8462fc410293256925618ff64a5b1efa870c0
lux <luna@lunalux.io>
2025-03-13 16:08:00 +0100

    dramatically improve implementation of partial using function signatures and binding

--- src/luxtools/functional/partial.py
+++ src/luxtools/functional/partial.py
@@ -1,9 +1,7 @@
 import functools
 import inspect
 import types
-from copy import deepcopy
 from functools import wraps
-from typing import Callable, List
 
 
 def copy_func(f):
@@ -19,7 +17,6 @@ def copy_func(f):
     g.__kwdefaults__ = f.__kwdefaults__
     return g
 
-
 def partial(f):
     """A wrapper for a user-defined function that allows for currying.
 
@@ -32,116 +29,36 @@ def partial(f):
     > foo(b=1)(1)(1) == 1
     > True
 
+    Notes:
+    Heavily uses:
+    - [Function signatures](https://peps.python.org/pep-0362/)
+
     Args:
         f (Callable): function to curry
 
     Returns:
         Callabe: The function itself with any arguments that were passed in curried.
     """
-    # f = copy_func(f)
 
     @wraps(f)
     def wrapper(*args, **kwargs):
-        fn = copy_func(f)
-
-        # Create a new function instance with its own state
-        required_parameters = len(
-            [
-                0
-                for _, param in fn._pargs.items()
-                if param.default == inspect.Parameter.empty
-            ]
-        )
-        given_parameters = len(args) + len(kwargs)
-
-        remaining_params = required_parameters
-
-        # update positional arguments
-        for i, arg in enumerate(args):
-            try:
-                key = next(
-                    k
-                    for k, v in fn._pargs.items()
-                    if v.default == inspect.Parameter.empty
-                )
-                fn._pargs[key] = inspect.Parameter(
-                    key, inspect.Parameter.KEYWORD_ONLY, default=arg
-                )
-            except StopIteration:
-                # handle *args parameter
-                if hasattr(fn, "_args"):
-                    fn._args.extend(args[i:])
-                    break
-                else:
-                    raise TypeError(
-                        f"{fn.__name__}() takes {required_parameters} positional arguments but {given_parameters} were given"
-                    )
-
-            remaining_params -= 1
-
-        # update keyword arguments
-        for key, value in kwargs.items():
-            # handle **kwargs parameter
-            if key not in fn._pargs:
-                if hasattr(fn, "_kwargs"):
-                    fn._kwargs[key] = value
-                else:
-                    raise TypeError(
-                        f"{fn.__name__}() got an unexpected keyword argument '{key}'"
-                    )
-
-            if fn._pargs[key].default == inspect.Parameter.empty:
-                # only update required args if we are adding new arguments instead of overriding existing ones.
-                remaining_params -= 1
-
-            # handle keyword arguments that are already matched
-            fn._pargs[key] = inspect.Parameter(
-                key, inspect.Parameter.KEYWORD_ONLY, default=value
-            )
-
-        # need to check that the given parameters are not keyword parameters that are already matched
-        do_currying = remaining_params != 0
-
-        if do_currying:
-            # do I even need c here, or can I just return partial(f)?
-            c = lambda *args, **kwargs: f(*args, **kwargs)  # noqa: E731
-            c._pargs = fn._pargs
-            if hasattr(fn, "_args"):
-                c._args = fn._args
-            if hasattr(f, "_kwargs"):
-                c._kwargs = fn._kwargs
-
-            # copy over attributes not handled by wraps
-            c.__doc__ = fn.__doc__
-            c.__name__ = fn.__name__
-
-            return partial(c)
-
-        if not hasattr(fn, "_args"):
-            fn._args = []
-        if not hasattr(fn, "_kwargs"):
-            fn._kwargs = {}
-
-        return fn(
-            **{
-                **{key: value.default for key, value in fn._pargs.items()},
-                **fn._kwargs,
-            }
-        )
-
-    # smuggle my monkey patched variables
-    if not hasattr(f, "_pargs"):
         sig = inspect.signature(f)
-        f._pargs = sig.parameters.copy()
+        total_parameters = len(sig.parameters)
 
-        if f._pargs.get("args"):
-            raise ValueError(
-                "We currently don't support *args in function as we convert all arguments to keyword only arguments."
-            )
-            f._pargs.pop("args")
-            f._args = []
-        if f._pargs.get("kwargs"):
-            f._pargs.pop("kwargs")
-            f._kwargs = {}
+        if not hasattr(f, "_pargs"):
+            f._pargs = {}  # initialize empty if none
 
-    return copy_func(wrapper)
+        bind = sig.bind_partial(*args, **{**f._pargs, **kwargs})
+        bind.apply_defaults()
+
+        applied_arguments = len(bind.arguments)
+
+        do_currying = applied_arguments < total_parameters
+        if do_currying:
+            fn = copy_func(f)
+            fn._pargs = bind.arguments.copy()
+            return partial(fn)
+
+        return f(*bind.args, **bind.kwargs)
+
+    return wrapper
