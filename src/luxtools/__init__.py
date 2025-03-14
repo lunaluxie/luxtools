fdf3945233e253e6961ab8c5e0694c4a9bb49bbd
lux <luna@lunalux.io>
2025-03-14 13:54:34 +0100

    modify imports

--- src/luxtools/__init__.py
+++ src/luxtools/__init__.py
@@ -1,6 +1,7 @@
 from .functional.compose import chain
+from .functional.overload import overload
 from .functional.partial import partial
-from .scientific.error_propagation import get_error, Variable
+from .scientific.error_propagation import get_error
 from .scientific.printing import NumericResult
 
-__all__ = ['chain', 'partial', 'get_error', 'Variable', 'NumericResult']
+__all__ = ["chain", "partial", "overload", "get_error", "NumericResult"]
--- src/luxtools/functional/__init__.py
+++ src/luxtools/functional/__init__.py
@@ -1,7 +1,5 @@
 from .compose import chain
+from .overload import overload
 from .partial import partial
 
-all = [
-    'chain',
-    'partial'
-]
+all = ["chain", "partial", "overload"]
